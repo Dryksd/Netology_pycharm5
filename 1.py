1717dd9ad1e8ede3a5a6a22818788e59efccdc08
Dryksd <samara.lawfirm@gmail.com>
2018-01-24 18:05:32 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,25 @@
+class non_human():
+    brain = 2
+
+class animals(non_human):
+    legs = 4
+    def power(meal):
+        strong = 0
+        if meal > 0:
+            strong == meal*2
+
+class bird (non_human):
+    legs = 2
+    def power (meal):
+        strong = 0
+        if meal > 0:
+            strong == meal*1.5
+
+cows = animals()
+goats = animals()
+sheep = animals()
+pigs = animals()
+
+chickens = bird()
+gooses = bird()
+ducks = bird()
